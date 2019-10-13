e0f3e37a86c7cf4eca929882a9dd5d2e707cd51a
Kancharla, Sai Krishna Teja <42943403+tejavictory@users.noreply.github.com>
2019-10-13 02:31:36 -0500

    Violation working

--- frontend/src/components/AddCodewordsModal.vue
+++ frontend/src/components/AddCodewordsModal.vue
@@ -8,7 +8,7 @@
             <div class="fields">
                 <div class="field">
                 <label>Codeword Set Name</label>
-                <input type="text" placeholder="Codeword Set Name" v-model="csname" @change="validateName()">
+                <input type="text" data-tooltip="testing" placeholder="Codeword Set Name" v-model="csname" @change="validateName()">
                 </div>
                 <div class="field" id="nameerr">
                     <label></label>
@@ -17,6 +17,17 @@
                 <div class="field" id="validname">
                     <label></label>
                         <i class="check icon green"></i>Name is available
+                </div>
+            </div>
+            <div class="field" id="violations">
+                <div class="ui message">
+                    <label>Violations</label>
+                    <ul>
+                        <li>Hard rule violations: {{ this.hardcount }}</li>
+                        <li>Soft rule violations: {{ this.softcount }}</li>
+                        <li> SOFT RULE VIOLATIONS ARE INDICATED IN RED TEXT. You can choose to override these and create the set.</li>
+                        <li> HARD RULE VIOLATIONS ARE INDICATED IN RED BACKGROUND. You cannot create a set unless you change these. </li>
+                    </ul>
                 </div>
             </div>
              <div class="ui stackable grid" id="grid">
@@ -46,18 +57,13 @@ export default {
     mounted() {
         $('#nameerr').hide()
         $('#validname').hide()
+        $('#violations').hide()
     },
     data() {
         return {
             csname: '',
-            cstartdate: '',
-            cenddate: '',
-            cpresurvey: '',
-            cpostsurvey: '',
-            cassignstat: '',
-            cstudents: [],
-            insEmail: '',
-            sets: '',
+            softcount: '',
+            hardcount: ''
         }
     },
     methods: {
@@ -89,13 +95,56 @@ export default {
             document.getElementById("grid").appendChild(node);
         },
         validateWords() {
+            $('#violations').hide()
             var c = document.getElementById("grid").childNodes;
             var word = "";
-            var i;
+            var i,j;
+            var words = new Array()
+            var softcount = 0
+            var hardcount = 0
             for (i = 0; i < c.length; i++) {
                 word = c[i].childNodes[0].value;
-                console.log(word)
+                c[i].childNodes[0].style.color = "#000000"
+                words.push(word.toUpperCase())
             }
+            for (i = 0; i < words.length - 1; i++){
+                for (j = i+1; j < words.length; j++){
+                    //hard rule
+                    if(this.compareTwoStrings(words[i],words[j]) > 0.7){
+                        document.getElementById("grid").childNodes[i].childNodes[0].style.color = "#ffffff"
+                        document.getElementById("grid").childNodes[j].childNodes[0].style.color = "#ffffff"
+                        document.getElementById("grid").childNodes[i].childNodes[0].style.backgroundColor = "#ff0000"
+                        document.getElementById("grid").childNodes[j].childNodes[0].style.backgroundColor = "#ff0000"
+                        hardcount++
+                        // console.log('Similar words: '+words[i]+', '+words[j])
+                    }
+                    //soft rule
+                    else if(this.compareTwoStrings(words[i],words[j]) > 0.5){
+                        document.getElementById("grid").childNodes[i].childNodes[0].style.color = "#ff0000"
+                        document.getElementById("grid").childNodes[j].childNodes[0].style.color = "#ff0000"
+                        document.getElementById("grid").childNodes[i].childNodes[0].style.backgroundColor = "#ffffff"
+                        document.getElementById("grid").childNodes[j].childNodes[0].style.backgroundColor = "#ffffff"  
+                        document.getElementById("grid").childNodes[i].setAttribute("data-tooltip","Soft rule")
+                        document.getElementById("grid").childNodes[j].setAttribute("data-tooltip","Soft rule")
+                        softcount++
+                    }
+                    //hard rule
+                    if(this.isAnagram(words[i],words[j])){
+                        // console.log('Anagrams: '+words[i]+', '+words[j])
+                        document.getElementById("grid").childNodes[i].childNodes[0].style.color = "#ffffff"
+                        document.getElementById("grid").childNodes[j].childNodes[0].style.color = "#ffffff"                        
+                        document.getElementById("grid").childNodes[i].childNodes[0].style.backgroundColor = "#ff0000"
+                        document.getElementById("grid").childNodes[j].childNodes[0].style.backgroundColor = "#ff0000"
+                        hardcount++
+                    }
+                }
+            }
+            if(softcount>0 || hardcount>0){
+                this.softcount = softcount
+                this.hardcount = hardcount
+                $('#violations').show()
+            }
+            console.log(words)
         },
         formatStudents: function () {
         },
@@ -127,6 +176,49 @@ export default {
                 // clear form inputs
                 this.cname = error
             })
+        },
+        compareTwoStrings(first, second) {
+            // first = first.replace(/\s+/g, '')
+            // second = second.replace(/\s+/g, '')
+
+            if (!first.length && !second.length) return 1;                   // if both are empty strings
+            if (!first.length || !second.length) return 0;                   // if only one is empty string
+            if (first === second) return 1;       							 // identical
+            if (first.length === 1 && second.length === 1) return 0;         // both are 1-letter strings
+            if (first.length < 2 || second.length < 2) return 0;			 // if either is a 1-letter string
+
+            let firstBigrams = new Map();
+            for (let i = 0; i < first.length - 1; i++) {
+                const bigram = first.substring(i, i + 2);
+                const count = firstBigrams.has(bigram)
+                    ? firstBigrams.get(bigram) + 1
+                    : 1;
+
+                firstBigrams.set(bigram, count);
+            };
+
+            let intersectionSize = 0;
+            for (let i = 0; i < second.length - 1; i++) {
+                const bigram = second.substring(i, i + 2);
+                const count = firstBigrams.has(bigram)
+                    ? firstBigrams.get(bigram)
+                    : 0;
+
+                if (count > 0) {
+                    firstBigrams.set(bigram, count - 1);
+                    intersectionSize++;
+                }
+            }
+
+            return (2.0 * intersectionSize) / (first.length + second.length - 2);
+        },
+        isAnagram (str1, str2) {
+            if (str1.length !== str2.length) {
+                return false;
+            }            
+            var sortStr1 = str1.split('').sort().join('');
+            var sortStr2 = str2.split('').sort().join('');
+            return (sortStr1 === sortStr2);
         }
     }
 }
@@ -139,5 +231,10 @@ export default {
 
     .field{
         padding: 10px;
+    }
+
+    .ui.message{
+        background-color: rgb(252, 228, 228);
+        color: red;
     }
 </style>
